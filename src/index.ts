3fc104a113a88258f128d44d6cb749bf3de0dc30
jd-solanki <jdsolanki0001@gmail.com>
2026-06-11 22:26:06 +0530

    feat: use update-notifier package to notify about update

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
 #!/usr/bin/env node
 import { Command } from 'commander'
+import updateNotifier from 'update-notifier'
 import { publishCommand } from './commands/publish.js'
 import { addCommand } from './commands/add.js'
 import { removeCommand } from './commands/remove.js'
 import { updateCommand } from './commands/update.js'
-import { maybeNotifyForUpdate } from './lib/update-notifier.js'
 import packageJson from '../package.json' with { type: 'json' }
+
+updateNotifier({ pkg: packageJson }).notify()
 
 const program = new Command('airig')
   .description('Manage project-scoped AI Setup artifacts')
@@ -15,11 +17,5 @@ program.addCommand(publishCommand)
 program.addCommand(addCommand)
 program.addCommand(updateCommand)
 program.addCommand(removeCommand)
-
-program.hook('postAction', () => {
-  try {
-    maybeNotifyForUpdate()
-  } catch {}
-})
 
 await program.parseAsync()
